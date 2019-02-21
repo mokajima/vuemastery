6fe6b82719c9f2b7bae34d200ad4f796cad56aed
mokajima <mokajima.85z@gmail.com>
2019-02-21 11:27:41 +0900

    feat: Listen for DOM events

--- intro-to-vue-js/main.js
+++ intro-to-vue-js/main.js
@@ -8,12 +8,23 @@ var app = new Vue({
     variants: [
       {
         variantId: 2234,
-        variantColor: 'green'
+        variantColor: 'green',
+        variantImage: './assets/vmSocks-green.jpg'
       },
       {
         variantId: 2235,
-        variantColor: 'blue'
+        variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue.jpg'
       }
-    ]
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
   }
 })
