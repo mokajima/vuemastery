8108bc8b8f21583680e392a38820350a11b04b50
mokajima <mokajima.85z@gmail.com>
2019-02-21 11:01:05 +0900

    feat: Display lists of a product's details

--- intro-to-vue-js/main.js
+++ intro-to-vue-js/main.js
@@ -3,6 +3,17 @@ var app = new Vue({
   data: {
     product: 'Socks',
     image: './assets/vmSocks-green.jpg',
-    inStock: true
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ]
   }
 })
