5a3cbbf08c38ab3107534bd9631f9111a451597a
mokajima <mokajima85z@gmail.com>
2019-03-02 16:00:29 +0900

    feat: Modify the cart to have products' ids

--- intro-to-vue-js/main.js
+++ intro-to-vue-js/main.js
@@ -58,7 +58,7 @@ Vue.component('product', {
   },
   methods: {
     addToCart() {
-      this.$emit('add-to-cart')
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
     },
     updateProduct(index) {
       this.selectedVariant = index
@@ -87,11 +87,11 @@ var app = new Vue({
   el: '#app',
   data: {
     premium: true,
-    cart: 0
+    cart: []
   },
   methods: {
-    updateCart() {
-      this.cart += 1
+    updateCart(id) {
+      this.cart.push(id)
     }
   }
 })
