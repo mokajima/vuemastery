0322bbda7ff8d49689e8496ad9166ffbfee8b3d5
mokajima <mokajima85z@gmail.com>
2019-03-24 12:23:31 +0900

    style: Break long lines into multiple lines

--- real-world-vue-js/src/components/EventCard.vue
+++ real-world-vue-js/src/components/EventCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <router-link class="event-link" :to="{ name: 'event-show', params: { id: event.id } }">
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: event.id } }"
+  >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.data }}</span>
       <h4 class="title">{{ event.title }}</h4>
