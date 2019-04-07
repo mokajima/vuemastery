4752b2f9a786dcbefe91817e8b1cb2f8258c00a9
mokajima <mokajima85z@gmail.com>
2019-04-07 19:11:55 +0900

    feat: Implement pagination

--- mastering-vuex/src/views/EventList.vue
+++ mastering-vuex/src/views/EventList.vue
@@ -2,6 +2,25 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page !== 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+      >
+        Prev Page
+      </router-link>
+    </template>
+    <template v-if="hasNextPage">
+      |
+    </template>
+    <template v-if="hasNextPage">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        rel="next"
+      >
+        Next Page
+      </router-link>
+    </template>
   </div>
 </template>
 
@@ -25,7 +44,10 @@ export default {
     page() {
       return parseInt(this.$route.query.page) || 1
     },
-    ...mapState(['events'])
+    hasNextPage() {
+      return this.eventsTotal > this.page * this.perPage
+    },
+    ...mapState(['events', 'eventsTotal'])
   }
 }
 </script>
