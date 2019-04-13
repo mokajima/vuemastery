313049190360e2d0b6c06d54b5c76dc48ab4bfa2
mokajima <mokajima85z@gmail.com>
2019-04-13 12:50:47 +0900

    refactor: Create an user Vuex module

--- mastering-vuex/src/store/store.js
+++ mastering-vuex/src/store/store.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import EventService from '@/services/EventService'
+import * as user from '@/store/modules/user'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    user
+  },
   state: {
-    user: { id: 'abc123', name: 'Adam Jahr' },
     categories: [
       'sustainability',
       'nature',
