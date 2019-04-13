ef7fde2e73c381ef753af1fae493cfd715232e98
mokajima <mokajima85z@gmail.com>
2019-04-13 13:45:16 +0900

    refactor: Namespace the event module

--- mastering-vuex/src/views/EventList.vue
+++ mastering-vuex/src/views/EventList.vue
@@ -35,7 +35,7 @@ export default {
   created() {
     this.perPage = 3
 
-    this.$store.dispatch('fetchEvents', {
+    this.$store.dispatch('event/fetchEvents', {
       perPage: this.perPage,
       page: this.page
     })
