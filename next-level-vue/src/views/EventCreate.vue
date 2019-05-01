6d03bc48a5d3370569631548fc7317006d499b20
mokajima <mokajima85z@gmail.com>
2019-05-01 14:19:58 +0900

    refactor: Add the BaseInput component

--- next-level-vue/src/views/EventCreate.vue
+++ next-level-vue/src/views/EventCreate.vue
@@ -8,21 +8,12 @@
       </select>
 
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title"/>
-      </div>
+      <BaseInput label="Title" v-model="event.title" type="text" placeholder="Title" class="field" />
 
-      <div class="field">
-        <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description"/>
-      </div>
+      <BaseInput label="Description" v-model="event.description" type="text" placeholder="Add a description" class="field" />
 
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location"/>
-      </div>
+      <BaseInput label="Location" v-model="event.location" type="text" placeholder="Add a location" class="field" />
 
       <h3>When is your event?</h3>
 
