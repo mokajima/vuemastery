657048858fbbc3969c952276745053ff49c4b8dc
mokajima <mokajima85z@gmail.com>
2019-05-02 17:01:49 +0900

    refactor: Add the formFieldMixin

--- next-level-vue/src/components/BaseSelect.vue
+++ next-level-vue/src/components/BaseSelect.vue
@@ -14,22 +14,14 @@
 </template>
 
 <script>
+import { formFieldMixin } from '../mixins/formFieldMixin'
+
 export default {
-  inheritAttrs: false,
+  mixins: [formFieldMixin],
   props: {
     options: {
       type: Array,
       required: true
-    },
-    label: {
-      type: String,
-      default: ''
-    },
-    value: [String, Number]
-  },
-  methods: {
-    updateValue(event) {
-      this.$emit('input', event.target.value)
     }
   }
 }
