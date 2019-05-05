82e3ed4d27b44899d95103c243aa70f5bbcf2293
mokajima <mokajima85z@gmail.com>
2019-05-05 14:54:06 +0900

    refactor: Use Async/Await instead of using Promises

--- scaling-vue-with-nuxt-js/pages/index.vue
+++ scaling-vue-with-nuxt-js/pages/index.vue
@@ -22,20 +22,18 @@ export default {
   components: {
     EventCard
   },
-  asyncData({ $axios, error }) {
-    return $axios
-      .get('http://localhost:3000/events')
-      .then(response => {
-        return {
-          events: response.data
-        }
+  async asyncData({ $axios, error }) {
+    try {
+      const response = await $axios.get('http://localhost:3000/events')
+      return {
+        events: response.data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. please try again.'
       })
-      .catch(e => {
-        error({
-          statusCode: 503,
-          message: 'Unable to fetch events at this time. please try again.'
-        })
-      })
+    }
   }
 }
 </script>
