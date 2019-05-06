2d73f1e19f99cce64da2047f9ee07105400349d1
mokajima <mokajima85z@gmail.com>
2019-05-06 15:58:30 +0900

    refactor: Move store.js to the vuex directory

--- token-based-authentication/src/vuex/store.js
+++ token-based-authentication/src/vuex/store.js
@@ -22,6 +22,18 @@ export default new Vuex.Store({
         .then(({ data }) => {
           commit('SET_USER_DATA', data)
         })
+    },
+    login ({ commit }, credentials) {
+      return axios
+        .post('//localhost:3000/login', credentials)
+        .then(({ data }) => {
+          commit('SET_USER_DATA', data)
+        })
+    }
+  },
+  getters: {
+    loggedIn (state) {
+      return !!state.user
     }
   }
 })
